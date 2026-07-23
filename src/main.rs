10d6ea7208677071f1e69b9ad838d4712ca55090
Akshit Gaur <akshitgaur2003@gmail.com>
2026-07-23 19:36:14 +0530

    Structs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
-struct User{
+struct User {
     name: String,
     email: String,
     active: bool,
-    id: u32
+    id: u32,
 }
 
 fn main() {
-    println!("Hello, world!");
-    //struct
-    let user1 = User { 
-        name: String::from("Akshit"),
-        email:String::from("akshit@gmail.com"),
-        active:true,
-        id:1
-    };
-
-    println!("Name : {}\n Email : {}", user1.name);
+    let users = vec![
+        User {
+            name: String::from("Akshit"),
+            email: String::from("akshit@gmail.com"),
+            active: true,
+            id: 1,
+        },
+        User {
+            name: String::from("Rahul"),
+            email: String::from("rahul@gmail.com"),
+            active: false,
+            id: 2,
+        },
+        User {
+            name: String::from("Raman"),
+            email: String::from("raman@gmail.com"),
+            active: true,
+            id: 3,
+        },
+        User {
+            name: String::from("Sid"),
+            email: String::from("sid@gmail.com"),
+            active: true,
+            id: 4,
+        },
+    ];
+    
+    for i in &users{
+        println!(
+            "Name: {}\nEmail: {}\nActive User: {}\nUser ID: {}\n",
+            i.name,
+            i.email,
+            i.active,
+            i.id
+        );
+    }
+ 
 }
